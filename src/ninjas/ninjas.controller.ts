2629e68cef2d7d4730f168d42ad8f1eb79cc7d02
Andrea Selmi <andrea@start2impact.com>
2024-05-23 16:25:58 +0200

    feat: add documentation

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -16,7 +16,7 @@ import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
 import { NinjasService } from './ninjas.service';
 import { BeltGuard } from 'src/belt/belt.guard';
-import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 
 @ApiTags("ninjas")
@@ -26,12 +26,16 @@ export class NinjasController {
 	constructor(private readonly ninjasService: NinjasService) { }
 
 	// GET /ninjas --> get all ninjas []
+	@ApiQuery({ name: 'weapon', enum: ['Hadouken', 'Shoryuken'], required: false })
+	@ApiOkResponse({ type: CreateNinjaDto, isArray: true, description: "Get all ninjas in our collection" })
+	@ApiNotFoundResponse({ description: "No ninjas found" })
 	@Get() // here we can define path like @Get('all') and the path will be /ninjas/all
-	getNinjas(@Query("weapon") weapon: "Hadouken" | "Shoryuken") {
+	getNinjas(@Query("weapon") weapon?: "Hadouken" | "Shoryuken") {
 		return this.ninjasService.getNinjas(weapon);
 	}
 
 	// GET /ninjas/:id --> get a single ninja
+	// Here I've used a pipe to parse the id to a number
 	@Get(':id')
 	getNinja(@Param('id', ParseIntPipe) id: number) {
 		try {
